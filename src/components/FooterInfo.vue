1916accb0320e939d24e0d8d259afdaaa103a6c1
Ahsan Zaman <ahsan,web.ml@gmail.com>
2019-01-20 14:21:46 +0300

    Frist Update

---
+++ src/components/FooterInfo.vue
@@ -0,0 +1,20 @@
+<template>
+<footer class="caption">
+  <p>Inspired by <a href="https://github.com/davidgaroro/vuetify-todo-pwa.git" target="_blank" rel="noopener">VuetifyTodoPWA</a></p>
+  <p><a href="https://github.com/davidgaroro/vuetify-todo-pwa" target="_blank" rel="noopener">GitHub Link</a></p>
+</footer>
+</template>
+
+<style lang="stylus">
+footer
+  margin: 65px auto 0
+  color: #bfbfbf
+  text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5)
+  p
+    line-height: 1
+  a
+    color: inherit
+    text-decoration: none
+    &:hover
+      text-decoration: underline
+</style>
---
+++ src/components/Share.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-btn
+    fixed
+    dark
+    fab
+    right
+    bottom
+    color="#00848C"
+    @click="share"
+  >
+    <v-icon>share</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  methods: {
+    async share() {
+      console.log('clicked')
+      try {
+        await navigator.share({ title: "Todo PWA", url: "" });
+        console.log("Data was shared successfully");
+      } catch (err) {
+        console.error("Share failed:", err.message);
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
